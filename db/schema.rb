acfb1f2df10028f844b278390a1fc1d97046ffac
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-27 10:30:40 -0500

    Update data type to description Col Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_145601) do
+ActiveRecord::Schema.define(version: 2020_03_27_152758) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

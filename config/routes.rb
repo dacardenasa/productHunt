20de15962f5dcbafe8c90d9a356dcf5491d6851f
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-02 09:03:27 -0500

    Anthenticate functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
   resources :users, only: [:new, :create]
-  # get 'products', to: 'products#index'
-  # post 'products', to: 'products#create'
-  # get 'products/new', to: 'products#new', as: 'new_product'
-  # get 'products/:id', to: 'products#show', as: 'product'
-  # get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
-  # patch 'products/:id',  to: 'products#update'
-  # delete 'products/:id', to: 'products#destroy'
 
   resources :products
   
-  root 'products#index'
+  
 
 end

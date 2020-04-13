07f77ece4a71bf61a868831e7ba7c1c5d4b19e7c
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-12 19:04:22 -0500

    Add credentials to AWS in environment/development.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      bucket: ENV.fetch('ph-diego'),
+      access_key_id: ENV.fetch('AKIAJ54KPZB5AI73MUWA'),
+      secret_access_key: ENV.fetch('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'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
